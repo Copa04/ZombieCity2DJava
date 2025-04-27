a16ecfd2364496181061de48a62e26db65dcde86
DavidCarausu <davidcarausu@gmail.com>
2025-04-27 16:18:05 +0300

    primul commit

--- src/Sound_Settings.java
+++ src/Sound_Settings.java
@@ -2,21 +2,21 @@ import javax.swing.*;
 
 public class Sound_Settings extends back_ground_image {
     public Menu_Buttons backButton;
-    JSlider MasterVolumeSlider;
-    JSlider MusicVolumeSlider;
-    JSlider CreaturesVolumeSlider;
+    TransparentSlider MasterVolumeSlider;
+    TransparentSlider MusicVolumeSlider;
+    TransparentSlider CreaturesVolumeSlider;
 
 
     public Sound_Settings() {
         this.setLayout(null);
         this.setBounds(0, 0, ct.FRAME_WIDTH, ct.FRAME_HEIGHT);
     //Label uri
-        JLabel settingsLabel = new JLabel("Sound Settings");
-        JLabel masterVolumeLabel = new JLabel("Master Volume");
-        JLabel creaturesVolumeLabel = new JLabel("Creatures Volume");
-        JLabel musicVolumeLabel = new JLabel("Music Volume");
+        Label_extend settingsLabel = new Label_extend("Sound Settings");
+        Label_extend masterVolumeLabel = new Label_extend("Master Volume");
+        Label_extend creaturesVolumeLabel = new Label_extend("Creatures Volume");
+        Label_extend musicVolumeLabel = new Label_extend("Music Volume");
 
-        settingsLabel.setBounds(10, 50, 100, 20);
+        settingsLabel.setBounds(10, 50, 200, 20);
         masterVolumeLabel.setBounds(ct.SLIDER_X_START-200, ct.SLIDER_Y_START, 300, 20);
         creaturesVolumeLabel.setBounds(ct.SLIDER_X_START-200, ct.SLIDER_Y_START+ct.SLIDER_SPACING, 300, 20);
         musicVolumeLabel.setBounds(ct.SLIDER_X_START-200, ct.SLIDER_Y_START+2*ct.SLIDER_SPACING, 300, 20);
@@ -29,13 +29,13 @@ public class Sound_Settings extends back_ground_image {
         back_ground_image img= new back_ground_image();
 
         //slidere
-        MasterVolumeSlider= new JSlider(JSlider.HORIZONTAL, 0, ct.SLIDER_VOLUME_MAX, ct.SLIDER_VOLUME_INITIAL);
+        MasterVolumeSlider= new TransparentSlider(JSlider.HORIZONTAL, 0, ct.SLIDER_VOLUME_MAX, ct.SLIDER_VOLUME_INITIAL);
         MasterVolumeSlider.setBounds(ct.SLIDER_X_START, ct.SLIDER_Y_START, ct.SLIDER_WIDTH, ct.SLIDER_HEIGHT);
 
-        CreaturesVolumeSlider= new JSlider(JSlider.HORIZONTAL, 0, ct.SLIDER_VOLUME_MAX, ct.SLIDER_VOLUME_INITIAL );
+        CreaturesVolumeSlider= new TransparentSlider(JSlider.HORIZONTAL, 0, ct.SLIDER_VOLUME_MAX, ct.SLIDER_VOLUME_INITIAL );
         CreaturesVolumeSlider.setBounds(ct.SLIDER_X_START,ct.SLIDER_Y_START+ct.SLIDER_SPACING, ct.SLIDER_WIDTH, ct.SLIDER_HEIGHT);
 
-        MusicVolumeSlider= new JSlider(JSlider.HORIZONTAL, 0, ct.SLIDER_VOLUME_MAX, ct.SLIDER_VOLUME_INITIAL );
+        MusicVolumeSlider= new TransparentSlider(JSlider.HORIZONTAL, 0, ct.SLIDER_VOLUME_MAX, ct.SLIDER_VOLUME_INITIAL );
         MusicVolumeSlider.setBounds(ct.SLIDER_X_START,ct.SLIDER_Y_START+2*ct.SLIDER_SPACING, ct.SLIDER_WIDTH, ct.SLIDER_HEIGHT);
 
         this.add(MusicVolumeSlider);
